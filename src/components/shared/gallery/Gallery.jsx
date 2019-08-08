aa44402cce20e9b7888fe9954fe12aafaa7d07d8
Ivan Ferenchuk <if@conciliolabs.com>
2019-08-08 23:32:37 +0300

    feat: gallery, styles

--- src/components/shared/gallery/Gallery.jsx
+++ src/components/shared/gallery/Gallery.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import shortId from 'shortid';
+import CloseIcon from './../../../icons/Close';
 
 class Gallery extends Component {
     constructor(props) {
@@ -7,6 +9,22 @@ class Gallery extends Component {
         this.state = {
             activeIndex: 0
         }
+
+        this.setActiveImage = this.setActiveImage.bind(this);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    }
+    
+
+    setActiveImage(index) {
+        this.setState({activeIndex: index})
+    }
+    
+
+    handleOutsideClick(e) {
+        if (!this.node.contains(e.target)) {
+            this.props.onCancel();
+            return;
+        }
     }
 
     render() {
@@ -14,14 +32,17 @@ class Gallery extends Component {
         const { activeIndex } = this.state;
 
         return (
-            <div className="modal">
-                <div className="modal-container">
-                    Modal Gallery
-                    <span onClick={this.props.onCancel}> Close</span>
-
+            <div className="modal animated fadeIn faster" onClick={this.handleOutsideClick}>
+                <div className="modal-container" ref={node => this.node = node}>
+                    <span className="modal-close" onClick={this.props.onCancel}><CloseIcon /></span>
+                    <div className="active-image">
+                    {images.map((image, index) => (
+                            <img src={image} key={shortId.generate()} className={activeIndex === index ? 'active animated fadeIn faster' : 'animated fadeOut faster'} alt="" />
+                        ))}
+                    </div>
                     <div className="thumbnails">
                         {images.map((image, index) => (
-                            <img src={image} className={activeIndex === index ? 'active' : ''} alt="" />
+                            <img src={image} key={shortId.generate()} onClick={() => (this.setActiveImage(index))} className={activeIndex === index ? 'active' : ''} alt="" />
                         ))}
                     </div>
                 </div>
